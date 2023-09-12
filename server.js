dd478f6838971151dede1986c76fcd37938706b5
JuanFranciscoGrillo <134559638+JuanFranciscoGrillo@users.noreply.github.com>
2023-09-12 09:14:04 -0500

    TOTAL RESET

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-// Import sequelize connection
-const sequelize = require('./config/connection'); // Assuming your Sequelize connection is exported from './config/connection'
+// import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,9 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// Sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-  });
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
 });
