4a677c00c03da33b221f4c5424afc9335c8f780e
JuanFranciscoGrillo <134559638+JuanFranciscoGrillo@users.noreply.github.com>
2023-09-12 10:56:46 -0500

    Imported Sequelize connection and synced models to database before server start.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const sequelize = require('./config/connection'); // Import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => { // force: false means it won't recreate tables if they already exist
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
